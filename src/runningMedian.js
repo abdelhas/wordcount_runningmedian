24535550623057deea286ab97c041d0d856ab8c1
abdelhas <abdelhas@iusb.edu>
2015-06-13 21:05:44 +0000

    fixed indents

--- src/runningMedian.js
+++ src/runningMedian.js
@@ -24,50 +24,52 @@ var content;
 var deferred = Q.defer();
 
 Q.all(getInputFiles(inputFolder).map(function(file) {
-    //console.log(file);
-    fs.readFile(inputFolder + "/" + file, function read(err, data) {
-	if (err) {
-        	deferred.reject(new Error(error));
-    	}
-    	content = data;
-        content.toString().split('\n').forEach(
-        function (line) {
-        
-          var words = line
-              .replace(/[.,?{}!$*\/\\\[\]~;():#"'-]/g, " ") // replace special characters
-              .replace(/\s+/g, " ") // normalize white space
-              .toLowerCase() // set to lower case
-              .split(" "); // split words
-          
-	  words = _.without(words, ''); // remove empty elements from array
-          
-	  if (words.length>0) { // check for empty lines
-          
-		linesLengths.push(words.length); // push line length to array
-	
-	 	runningMedian = median(linesLengths).toFixed(1); // calculate median
-        
-	  	fs.appendFile(outputFile, runningMedian + "\n", function (err) { // output median to result file
-                	if (err) throw err;
-          	});
-	  };
-        });
-    });
-}))
-.then(function (result) {
-    // Done
+        //console.log(file);
+        fs.readFile(inputFolder + "/" + file, function read(err, data) {
+            if (err) {
+                deferred.reject(new Error(error));
+            }
+            content = data;
+            content.toString().split('\n').forEach(
+                function(line) {
 
-});
+                    var words = line
+                        .replace(/[.,?{}!$*\/\\\[\]~;():#"'-]/g, " ") // replace special characters
+                        .replace(/\s+/g, " ") // normalize white space
+                        .toLowerCase() // set to lower case
+                        .split(" "); // split words
+
+                    words = _.without(words, ''); // remove empty elements from array
+
+                    if (words.length > 0) { // check for empty lines
+
+                        linesLengths.push(words.length); // push line length to array
+
+                        runningMedian = median(linesLengths).toFixed(1); // calculate median
+
+                        fs.appendFile(outputFile, runningMedian + "\n", function(err) { // output median to result file
+                            if (err) throw err;
+                        });
+                    };
+                });
+        });
+    }))
+    .then(function(result) {
+        // Done
+
+    });
 
 // calculate median function
 function median(values) {
 
-    values.sort( function(a,b) {return a - b;} );
+    values.sort(function(a, b) {
+        return a - b;
+    });
 
-    var half = Math.floor(values.length/2);
+    var half = Math.floor(values.length / 2);
 
-    if(values.length % 2)
+    if (values.length % 2)
         return values[half];
     else
-        return (values[half-1] + values[half]) / 2.0;
+        return (values[half - 1] + values[half]) / 2.0;
 }
--- src/wordCount.js
+++ src/wordCount.js
@@ -20,45 +20,44 @@ var wordCount = {};
 
 
 Q.all(getInputFiles(inputFolder).map(function(file) {
-    // read files
-    fs.readFileSync(inputFolder + "/" + file).toString().split('\n').forEach(
-        function (line) { 
-          // get lines
-	  var words = line
-              .replace(/[.,?{}!$*\/\\\[\]~;():#"'-]/g, " ")
-              .replace(/\s+/g, " ")
-              .toLowerCase()
-              .split(" ");
-	  // add counts of words to object
-	  words.forEach(function (word) {
-	    // set word key 
-            if (!(wordCount.hasOwnProperty(word)) && word !== '') {
-              wordCount[word] = 0;
-            };
-	    // increment word occurence
-            if (word !== '') { 
-		wordCount[word]++;
-	    };
-    	  });
-	});
-    
-    return 'Word Count task complete.';
-}))
-.done(function (result) {
-    // words sort then output to result file
-    Object.keys(wordCount)
-      .sort()
-      .forEach(function (key) {
-        if (key.length > 5) {
-	  fs.appendFile(outputFile, key + '   	' + wordCount[key] + "\n", function (err) {
-    		if (err) throw err;
-	  });
-	} else { 
-	  fs.appendFile(outputFile, key + '	   	' + wordCount[key] + "\n", function (err) {
-    		if (err) throw err;
-	  });	
-	};
-    });
-  // Done
-});
+        // read files
+        fs.readFileSync(inputFolder + "/" + file).toString().split('\n').forEach(
+            function(line) {
+                // get lines
+                var words = line
+                    .replace(/[.,?{}!$*\/\\\[\]~;():#"'-]/g, " ")
+                    .replace(/\s+/g, " ")
+                    .toLowerCase()
+                    .split(" ");
+                // add counts of words to object
+                words.forEach(function(word) {
+                    // set word key 
+                    if (!(wordCount.hasOwnProperty(word)) && word !== '') {
+                        wordCount[word] = 0;
+                    };
+                    // increment word occurence
+                    if (word !== '') {
+                        wordCount[word]++;
+                    };
+                });
+            });
 
+        return 'Word Count task complete.';
+    }))
+    .done(function(result) {
+        // words sort then output to result file
+        Object.keys(wordCount)
+            .sort()
+            .forEach(function(key) {
+                if (key.length > 5) {
+                    fs.appendFile(outputFile, key + '   	' + wordCount[key] + "\n", function(err) {
+                        if (err) throw err;
+                    });
+                } else {
+                    fs.appendFile(outputFile, key + '	   	' + wordCount[key] + "\n", function(err) {
+                        if (err) throw err;
+                    });
+                };
+            });
+        // Done
+    });
